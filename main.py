ecea1b6796688a67bab28a55fc7d4d5742762175
vanyavorobiov <s19b1_vorobiov@179.ru>
2024-08-26 16:47:24 +0300

    user info

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    user_id = update.message.from_user.id
-    save_user_id(user_id)
-    await update.message.reply_text('Привет! Я переписываю твои сообщения ЗаБоРоМ.')
+    user = update.message.from_user
+    user_id = user.id
+    username = user.username if user.username else 'No username'
+    first_name = user.first_name if user.first_name else 'No first name'
+    last_name = user.last_name if user.last_name else 'No last name'
+
+    save_user_info(user_id, username, first_name, last_name)
+    await update.message.reply_text(f'Привет, {first_name}! Я переписываю твои сообщения ЗаБоРоМ.')
+
+
+def save_user_info(user_id: int, username: str, first_name: str, last_name: str):
+    with open("user_info.txt", "a") as file:
+        file.write(f"ID: {user_id}, Username: {username}, First Name: {first_name}, Last Name: {last_name}\n")
 
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     original_text = update.message.text
     transformed_text = transform_text(original_text)
     await update.message.reply_text(transformed_text)
+
 
 def transform_text(text: str) -> str:
     result = []
@@ -26,9 +38,6 @@ def transform_text(text: str) -> str:
             result.append(char)
     return ''.join(result)
 
-def save_user_id(user_id: int):
-    with open("user_ids.txt", "a") as file:
-        file.write(f"{user_id}\n")
 
 def main():
     token = '***REMOVED***'
@@ -36,6 +45,7 @@ def main():
     application.add_handler(CommandHandler("start", start))
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
     application.run_polling()
+
 
 if __name__ == '__main__':
     main()
