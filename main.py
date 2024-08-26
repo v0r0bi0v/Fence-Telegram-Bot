255b398bf0047a7d05919b640efebf0c939dab5f
vanyavorobiov <s19b1_vorobiov@179.ru>
2024-08-26 16:11:41 +0300

    new version of telegram

--- main.py
+++ main.py
@@ -1,39 +1,33 @@
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 
 
 # Функция, которая будет обрабатывать команды /start и /help
-def start(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('Привет! Я бот, который повторяет все, что ты скажешь.')
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text('Привет! Я бот, который повторяет все, что ты скажешь.')
 
 
 # Функция, которая будет повторять сообщения
-def echo(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text(update.message.text)
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(update.message.text)
 
 
 # Основная функция для запуска бота
 def main():
     # Вставьте сюда ваш токен, который вы получили от BotFather
-    token = '***REMOVED***'
+    token = 'YOUR_TOKEN_HERE'
 
-    # Создаем экземпляр класса Updater и передаем ему токен
-    updater = Updater(token)
-
-    # Получаем диспетчер для регистрации обработчиков
-    dispatcher = updater.dispatcher
+    # Создаем экземпляр класса Application и передаем ему токен
+    application = Application.builder().token(token).build()
 
     # Регистрируем обработчик команд /start и /help
-    dispatcher.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("start", start))
 
     # Регистрируем обработчик всех текстовых сообщений
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
 
     # Запускаем бота
-    updater.start_polling()
-
-    # Работаем до прерывания
-    updater.idle()
+    application.run_polling()
 
 
 if __name__ == '__main__':
