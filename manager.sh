a282ba5bca7984ff28f8d70ef1b4cc54dd016337
Ubuntu <ubuntu@ov-ff7805.infomaniak.ch>
2025-04-24 17:20:37 +0000

    manager.sh

---
+++ manager.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Переходим в директорию проекта
+cd ~/projects/Fence-Telegram-Bot || exit
+
+# Функция для запуска бота
+start_bot() {
+    if pgrep -f "python3 main.py --bot fence" > /dev/null; then
+        echo "Fence-бот уже запущен."
+    else
+        nohup python3 main.py --bot fence > fence_bot.log 2>&1 &
+        echo "Gym-бот запущен."
+    fi}
+
+
+# Останавливаем все процессы Gym-бота
+stop_all() {
+    pkill -f "python3 main.py --bot gym"
+    echo "Все процессы Fence-бота остановлены."
+}
+
+# Проверяем аргументы командной строки
+case "$1" in
+    start)
+        start_bot
+        ;;
+    stop)
+        stop_all
+        ;;
+    *)
+        echo "Использование: $0 {start|stop}"
+        exit 1
+        ;;
+esac
+
+exit 0
